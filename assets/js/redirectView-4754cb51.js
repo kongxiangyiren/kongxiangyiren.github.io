cae18f2f230f93fff3be67e90a42cd78a46192e1
kongxiangyiren <72970235+kongxiangyiren@users.noreply.github.com>
2022-11-10 11:32:09 +0000

    Deploying to gh-pages from @ kongxiangyiren/kongxiangyiren.github.io@aa225677e5c9920a7a6f9af9ace825f0183c1067 🚀

---
+++ assets/js/redirectView-4754cb51.js
@@ -0,0 +1,1 @@
+import{d as e,u as t}from"./index-cee673e7.js";const r=e({__name:"redirectView",setup(e){var r;const a=t();return"http"===(null==(r=a.query.path)?void 0:r.slice(0,4))&&(location.href=a.query.path),(e,t)=>null}});export{r as default};
