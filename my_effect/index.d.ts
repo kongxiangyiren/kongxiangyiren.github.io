ee33ca06c1ee4818a37ad7e6254f7fd86cdcf788
空巷一人 <1530688385@qq.com>
2022-12-21 16:00:58 +0800

    1

--- my_effect/index.d.ts
+++ my_effect/index.d.ts
@@ -5,16 +5,3 @@
  * @return {void}
  */
 export declare function clicker(love: Boolean, value: Boolean): void;
-/**
- * @description: 默哀，网站灰色
- * @param {Array} mourning 默哀,默认 每年12月13日00:00:00-每年12月14日00:00:00 网站灰色
- * let year = new Date().getFullYear(); //年
- * mourning([
- * {
- * startingTime: year + '-12-13 00:00:00',
- * timeEnd: year + '-12-14 00:00:00'
- * }
- * ]);
- * @return {void}
- */
-export declare function mourning(mourning: [{ startingTime: string; timeEnd: string }]): void;
--- src/main.ts
+++ src/main.ts
@@ -4,17 +4,12 @@ import App from './App.vue';
 import router from './router';
 import vueWechatTitle from 'vue-wechat-title';
 // 点击效果
-import { clicker, mourning } from 'my_effect';
+import { clicker } from 'my_effect';
 //鼠标点击
 clicker(false, true);
 // 默哀
 let year = new Date().getFullYear(); //年
-mourning([
-  {
-    startingTime: year + '-12-13 00:00:00',
-    timeEnd: year + '-12-14 00:00:00'
-  }
-]);
+
 const app = createApp(App);
 app.use(router).use(vueWechatTitle);
 
