69d4102e1206c8d4efd12bfe74864fd48df42e78
空巷一人 <1530688385@qq.com>
2023-01-10 14:40:12 +0800

    1

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,9 @@ import App from './App.vue';
 import router from './router';
 import vueWechatTitle from 'vue-wechat-title';
 // 点击效果
-import { clicker } from 'my_effect';
+import clicker from 'my-clicker';
 //鼠标点击
-clicker(false, true);
-// 默哀
-let year = new Date().getFullYear(); //年
+clicker(false, true, false, false);
 
 const app = createApp(App);
 app.use(router).use(vueWechatTitle);
